d9bc3821efa3afc1fc5035be24caa6f238e29bc7
soyiss <zwcrvl@naver.com>
2023-06-02 16:58:19 +0900

    springboot member 비밀번호 정규식 axios로 도전중..

--- src/main/java/com/example/member/controller/MemberController.java
+++ src/main/java/com/example/member/controller/MemberController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Controller
 @RequiredArgsConstructor
@@ -60,6 +62,7 @@ public class MemberController {
         session.setAttribute("loginEmail", memberDTO.getMemberEmail());
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
     @GetMapping("/axios/{id}")
     public ResponseEntity detailAxios(@PathVariable Long id) throws Exception {
         MemberDTO memberDTO = memberService.findById(id);
@@ -74,18 +77,18 @@ public class MemberController {
     }
 
     @GetMapping("/update")
-    public String updateForm(HttpSession session,Model model){
+    public String updateForm(HttpSession session, Model model) {
         String loginEmail = (String) session.getAttribute("loginEmail");
 
         MemberDTO memberDTO = memberService.findByEmail(loginEmail);
-        model.addAttribute("memberDTO",memberDTO);
+        model.addAttribute("memberDTO", memberDTO);
         //위에 두줄대신 이렇게 써도된다
 //        model.addAttribute("memberDTO",memberService.findByEmail(loginEmail));
         return "/memberPages/memberUpdate";
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity update(@RequestBody MemberDTO memberDTO ){
+    public ResponseEntity update(@RequestBody MemberDTO memberDTO) {
         memberService.update(memberDTO);
         return new ResponseEntity<>(HttpStatus.OK);
     }
@@ -98,27 +101,44 @@ public class MemberController {
     }
 
     @GetMapping("/{id}")
-    public String detail(@PathVariable Long id,Model model){
+    public String detail(@PathVariable Long id, Model model) {
         MemberDTO memberDTO = memberService.findById(id);
         model.addAttribute("member", memberDTO);
         return "memberPages/memberDetail";
     }
+
     @GetMapping("/mypage")
     public String myPage() {
         return "memberPages/memberMain";
     }
 
     @PostMapping("/dup-check")
-    public ResponseEntity emailCheck(@RequestBody MemberDTO memberDTO ){
+    public ResponseEntity emailCheck(@RequestBody MemberDTO memberDTO) {
 //        memberService.findByEmail(memberDTO.getMemberEmail());
         boolean result = memberService.emailCheck(memberDTO.getMemberEmail());
-        if(result){
+        if (result) {
             return new ResponseEntity<>(HttpStatus.OK);
-        }else{
-            return  new ResponseEntity<>(HttpStatus.CONFLICT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+
         }
 
     }
 
-
+    /*
+        1. Pattern.compile(exp)을 사용하여 정규식 패턴을 컴파일합니다.
+        2. pattern.matcher(memberDTO.getMemberPassword())를 통해 memberDTO 객체의 비밀번호를 정규식 패턴과 비교할 수 있는 Matcher 객체를 생성합니다.
+        3. matcher.matches()를 호출하여 비밀번호가 정규식 패턴과 일치하는지를 확인합니다. 일치할 경우 true를 반환하고, 그렇지 않을 경우 false를 반환합니다.
+        4. 확인 결과를 반환합니다.
+        5. 따라서 클라이언트는 /pass-rule 엔드포인트로 POST 요청을 보내고, 요청 본문에 회원 비밀번호를 포함시키면 서버는 해당 비밀번호가 정규식 규칙을 만족하는지를 확인하고,
+           그 결과를 boolean 값으로 반환합니다. 이를 통해 클라이언트는 회원 비밀번호의 유효성을 검증할 수 있습니다.
+    */
+    @PostMapping("/pass-rule")
+    public boolean passRule(@RequestBody MemberDTO memberDTO) {
+        String exp = "^(?=.*[a-z])(?=.*\\d)(?=.*[-_!#])[A-Za-z\\d-_!#]{5,10}$";
+        Pattern pattern = Pattern.compile(exp);
+        Matcher matcher = pattern.matcher(memberDTO.getMemberPassword());
+        boolean isValid = matcher.matches();
+        return isValid;
+    }
 }
